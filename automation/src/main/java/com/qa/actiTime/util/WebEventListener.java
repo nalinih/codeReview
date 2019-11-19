f28fb8e0e06747c6954a2dacfe9b4963b86532b8
nalinih <55223197+nalinih@users.noreply.github.com>
2019-11-20 05:13:55 +0530

    test util

---
+++ automation/src/main/java/com/qa/actiTime/util/WebEventListener.java
@@ -0,0 +1,5 @@
+package com.qa.actiTime.util;
+
+public class WebEventListener {
+
+}
--- automation/src/test/java/com/qa/actiTime/testCases/ProfilePageTest.java
+++ automation/src/test/java/com/qa/actiTime/testCases/ProfilePageTest.java
@@ -1,5 +1,16 @@
 package com.qa.actiTime.testCases;
 
-public class ProfilePageTest {
+import org.testng.annotations.DataProvider;
+
+import com.qa.actiTime.TestBase.TestBase;
+import com.qa.actiTime.util.TestUtil;
+
+public class ProfilePageTest extends TestBase{
+	
+	@DataProvider
+	public Object[][]  getActData(){
+		Object data [][]= TestUtil.getTestData("Users");
+		return data;
+	}
 
 }
